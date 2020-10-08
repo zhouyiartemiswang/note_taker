d39806f3fbc58c389c351613a830900a2d096bd0
Artemis <wzyzhouyi@gmail.com>
2020-10-07 23:39:04 -0700

    added comments

--- server.js
+++ server.js
@@ -13,72 +13,88 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// HTML Routes
-// ------------------------------------------------------------------------
-
 // Notes page route
 app.get("/notes", function(req, res) {
+    // Direct to notes.html
     res.sendFile(path.join(__dirname, "public/notes.html"));
-    // console.log(path.join(__dirname, "public/notes.html"));
 });
 
-// API Routes
-// ------------------------------------------------------------------------
-
-// Notes page route
+// API notes page route
 app.get("/api/notes", function(req, res) {
 
     // Read data file
     fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
         if (err) throw err;
-        // console.log(JSON.parse(data)[0]);
-        return res.json(JSON.parse(data));
+
+        // Send all data from db.json to client
+        res.json(JSON.parse(data));
     });
 
 });
 
-// Home page route
+// Home page route, if anything other than /notes and /api/notes are entered in url, direct to home page
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
+// API notes route
 app.post("/api/notes", function(req, res) {
+
+    // Read data file 
     fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
+
         if (err) throw err;
 
+        // Create an array to house data in db.json
         let notesData = JSON.parse(data) || [];
+
+        // Create an object to house incoming data
         let newData = req.body;
 
+        // If nothing in db.json
         if (notesData.length === 0) {
+            // Then incoming data id = 1
             newData.id = 1;
-        } else {
+        } 
+        // Otherwise, the id will be the last object's id + 1
+        else {
             newData.id = notesData[notesData.length - 1].id + 1;
         }
 
+        // Add incoming data to stored data array
         notesData.push(newData);
-        // console.log(notesData);
+
+        // Write new data array to db.json
         fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(notesData, null, 4), err => {
             if (err) throw err;
-            console.log("writing complete");
         })
+
     });
 
-    return res.send(req.body);
+    res.send(req.body);
 });
 
+// API notes route
 app.delete("/api/notes/:id", function(req, res) {
+
+    // Read data file
     fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
+
         if (err) throw err;
+
+        // Filter out the object with id specified by user 
         let newNotesData = JSON.parse(data).filter(function(currentNote) {
             return currentNote.id != req.params.id;
         });
-        console.log(newNotesData);
+
+        // Write the filtered data to db.json
         fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(newNotesData, null, 4), err => {
             if (err) throw err;
-            console.log("writing complete");
         })
+
     });
     res.send("deleted");
+
 });
 
 // Start server to begin listening
