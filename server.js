f43ad062709928619ddad1bb5f2dbbd060c8345d
Artemis <wzyzhouyi@gmail.com>
2020-10-07 22:21:21 -0700

    added get and post routes

---
+++ server.js
@@ -0,0 +1,63 @@
+// Set up dependencies
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+// Set up the Express App
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Set up the Express App to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// HTML Routes
+// ------------------------------------------------------------------------
+
+// Notes page route
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+    // console.log(path.join(__dirname, "public/notes.html"));
+});
+
+// API Routes
+// ------------------------------------------------------------------------
+
+// Notes page route
+app.get("/api/notes", function(req, res) {
+
+    // Read data file
+    fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
+        if (err) throw err;
+        console.log(JSON.parse(data)[0]);
+        return res.json(JSON.parse(data));
+    });
+
+});
+
+// Home page route
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.post("/api/notes", function(req, res) {
+    fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
+        if (err) throw err;
+        let notesData = JSON.parse(data);
+        notesData.push(req.body);
+        console.log(notesData);
+        fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(notesData, null, 4), err => {
+            if (err) throw err;
+            console.log("writing complete");
+        })
+    });
+
+    return res.send(req.body);
+});
+
+// Start server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
